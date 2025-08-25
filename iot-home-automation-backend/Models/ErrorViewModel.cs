5619c21676866e1cdef4d4521e08ab2fba740a3d
Eebbaa <eebbaadh@gmail.com>
2025-08-25 14:28:55 +0200

    Add project files.

---
+++ iot-home-automation-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iot_home_automation_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
