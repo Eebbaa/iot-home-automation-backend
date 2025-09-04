68692e7d580dfb58ef706fe19ed4dc996fd079f5
Eebbaa <eebbaadh@gmail.com>
2025-09-04 14:50:44 +0200

    chore: remove Docker setup and switch to LocalDB
    - Deleted Dockerfile and docker-compose since containerized SQL Server was causing connection issues
    - Configured connection string to use LocalDB (mssqllocaldb) instance on Windows
    - Verified DB accessibility via SQL Server Management Studio
    -Register works fine while testing, but got error while login to fix the issue the following tasks are done.
    fix: configure IdentityDbContext to resolve login DbSet issue and DI errors
    
    - Updated ApplicationDbContext to inherit from IdentityDbContext<User> instead of IdentityUser
    - Removed redundant DbSet<User> since Identity manages users
    - Registered Identity services in Program.cs with AddIdentity<User, IdentityRole>() and EntityFrameworkStores
    - Updated AuthController constructor to use UserManager<User> and SignInManager<User>
    - Added migration to create necessary ASP.NET Identity tables in the database
    - This fixes login errors caused by missing IdentityUserClaim, other Identity tables, and DI issues

--- iot-home-automation-backend/Data/ApplicationDbContext.cs
+++ iot-home-automation-backend/Data/ApplicationDbContext.cs
@@ -1,15 +1,17 @@
 ﻿using iot_home_automation_backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace iot_home_automation_backend.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         { 
         }
-    
-    public DbSet<User> Users { get; set; } = null!;
+    //Identity will manage to create the user Table no need to mention it here.
+   // public DbSet<User> Users { get; set; } = null!;
     public DbSet<Device> Devices { get; set; } = null!;
     public DbSet<DeviceReading> DeviceReadings { get; set; } = null!;
 
