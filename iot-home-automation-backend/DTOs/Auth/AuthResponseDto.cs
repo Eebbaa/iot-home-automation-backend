b9226b2a4c94459898404f727388b610de2cca1f
Eebbaa <eebbaadh@gmail.com>
2025-09-09 22:36:08 +0200

    feat(auth): add password recovery and reset functionality
    - Added ForgotPassword API to generate password reset token
    - Added ResetPassword API to validate token and update user password
    - Added DTOs: ForgotPasswordDto and ResetPasswordDto
    - Integrated with ASP.NET Core Identity's token generation and reset APIs
    - added with error handling and responses for invalid email, token, or password

--- iot-home-automation-backend/DTOs/Auth/AuthResponseDto.cs
+++ iot-home-automation-backend/DTOs/Auth/AuthResponseDto.cs
@@ -1,8 +1,25 @@
-﻿namespace iot_home_automation_backend.DTOs.Auth
+﻿
+namespace iot_home_automation_backend.DTOs.Auth
 {
     public class AuthResponseDto
     {
+        internal IEnumerable<string> Errors;
+
         public string Message { get; set; }
         public string Token { get; set; }
     }
+
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; }
+
+    }
+    public class ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+
+    }
 }
+
